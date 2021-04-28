236cbe31aaf2fbfc48012b675f50aa99d22addb2
Shane32 <shane@acdmail.com>
2021-04-28 11:51:00 -0400

    Update

--- src/GraphQL.Relay/Properties/AssemblyInfo.cs
+++ src/GraphQL.Relay/Properties/AssemblyInfo.cs
@@ -5,9 +5,6 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("GraphQL.Relay")]
 [assembly: AssemblyTrademark("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
